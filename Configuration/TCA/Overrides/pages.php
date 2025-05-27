a94f6a10700983c0b9ebd3c75e3f4ca64ccecb91
andreasloewer <info@andreasloewer.de>
2025-05-27 13:57:23 +0200

    no message

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,18 +1,43 @@
 <?php
-defined('TYPO3') or die('Access denied.');
-call_user_func(function()
-{
-    /**
-     * Temporary variables
-     */
-    $extensionKey = 'page_images';
 
-    /**
-     * Default PageTS for PageImages
-     */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        'Configuration/TsConfig/Page/All.tsconfig',
-        'Page Images'
-    );
-});
+defined('TYPO3') or die('Access denied.');
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+// Neue Felder definieren
+$additionalColumns = [
+    'tx_page_image' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:page_overview/Resources/Private/Language/locallang_db.xlf:pages.tx_page_overview_image',
+        'config' => ExtensionManagementUtility::getFileFieldTCAConfig(
+            'tx_page_image',
+            [
+                'appearance' => [
+                    'createNewRelationLinkTitle' => 'Bild hinzufügen',
+                    'showPossibleLocalizationRecords' => true,
+                ],
+                'maxitems' => 1,
+            ],
+            'jpg,jpeg,png,svg,gif' // Erlaubte Bildtypen
+        ),
+    ],
+];
+
+// Felder registrieren
+ExtensionManagementUtility::addTCAcolumns('pages', $additionalColumns);
+
+// Felder dem Backend-Formular hinzufügen
+ExtensionManagementUtility::addToAllTCAtypes(
+    'pages',
+    'tx_page_image',
+    '',
+    'after:title'
+);
+
+// PageTSConfig einbinden (optional)
+ExtensionManagementUtility::registerPageTSConfigFile(
+    'page_images',
+    'Configuration/TsConfig/Page/All.tsconfig',
+    'Page Images'
+);
